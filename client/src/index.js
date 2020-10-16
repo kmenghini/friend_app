1fe0e60aa1bdd8eeea761f6a355334ff7efb97cc
Kaitlyn Menghini <kaitlyn.menghini@gmail.com>
2020-10-16 12:57:25 -0700

    Add bootstrap dependency

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './app/app';
 import * as serviceWorker from './serviceWorker';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
